59b5f98bc5a073a43802a53016abe77a4ea0be82
root <linhnh@twin.vn>
2016-03-16 00:38:20 +0700

    Add react-native-maps package

--- app/post-task/step-2.js
+++ app/post-task/step-2.js
@@ -2,14 +2,50 @@ import React, {
   StyleSheet,
   View,
   Text,
+  Image,
 } from 'react-native';
 
+import MapView from 'react-native-maps';
+
 export default React.createClass({
+  onRegionChange(region) {
+    
+  },
+
   render() {
     return (
-      <View>
-        <Text>Step 2 {this.props.route.serviceId}</Text>
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 10.720754,
+            longitude: 106.729579,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          onRegionChange={this.onRegionChange}
+        />
+        <Image style={styles.getLocationImg} source={require('../img/marker.png')} />
       </View>
     );
   }
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+  getLocationImg: {
+    marginBottom: 60,
+  },
 });
